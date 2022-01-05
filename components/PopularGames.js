1fd6153e19e5a0d112e69951e7a8394670fb981c
Saket Khare <saketkhare2000@gmail.com>
2022-01-06 00:55:16 +0530

    spinner fixed

--- components/PopularGames.js
+++ components/PopularGames.js
@@ -3,15 +3,20 @@ import data from "../data.json";
 import { popularGamesURL } from "../api";
 import axios from "axios";
 import GameCard from "./GameCard";
+import Spinner from "./Spinner";
 
 const PopularGames = () => {
+  //loading
+  const [loading, setLoading] = React.useState(false);
   //axios call
   const [popularGames, setPopularGames] = React.useState([]);
   React.useEffect(() => {
+    setLoading(true);
     axios
       .get(popularGamesURL())
       .then((res) => {
         setPopularGames(res.data.results);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
@@ -23,18 +28,25 @@ const PopularGames = () => {
       <h1 className="text-5xl font-bold text-white font-heading my-10">
         Popular Games
       </h1>
-      <div className="flex w-full justify-between flex-wrap">
-        {
-          //map through popularGames array
-          popularGames.map((game) => {
-            return game.background_image === null ? (
-              <></>
-            ) : (
-              <GameCard game={game} />
-            );
-          })
-        }
-      </div>
+      {
+        //loading
+        loading ? (
+          <Spinner />
+        ) : (
+          <div className="flex w-full justify-between flex-wrap">
+            {
+              //map through popularGames array
+              popularGames.map((game) => {
+                return game.background_image === null ? (
+                  <></>
+                ) : (
+                  <GameCard game={game} />
+                );
+              })
+            }
+          </div>
+        )
+      }
     </div>
   );
 };
